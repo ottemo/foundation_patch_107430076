8d653517f037d3075b4455799f2440f1183e68e4
Oleksij <obesarab@speroteck.com>
2016-07-05 20:23:43 +0300

    fix for snake_casing in subscription keys (#1)

--- patch.go
+++ patch.go
@@ -10,7 +10,9 @@ import (
 
 	// using standard set of packages
 
+	"github.com/ottemo/foundation/app/models/cart"
 	"github.com/ottemo/foundation/app/models/product"
+	"github.com/ottemo/foundation/app/models/subscription"
 	_ "github.com/ottemo/foundation/basebuild"
 	"github.com/ottemo/foundation/env"
 )
@@ -25,6 +27,7 @@ func init() {
 func main() {
 	defer app.End() // application close event
 
+	fmt.Println("starting application " + time.Now().String())
 	// application start event
 	if err := app.Start(); err != nil {
 		env.LogError(err)
@@ -37,26 +40,52 @@ func main() {
 	productCollection, err := product.GetProductCollectionModel()
 	if err != nil {
 		fmt.Println(env.ErrorDispatch(err))
-
 	}
 
+	fmt.Println("update products options" + time.Now().String())
 	// update products option
 	for _, currentProduct := range productCollection.ListProducts() {
 		newOptions := ConvertProductOptionsToSnakeCase(currentProduct)
 		err = currentProduct.Set("options", newOptions)
 		if err != nil {
 			fmt.Println(env.ErrorDispatch(err))
-
 		}
 
 		err := currentProduct.Save()
 		if err != nil {
 			fmt.Println(env.ErrorDispatch(err))
-
 		}
-
 	}
 
+	// get product collection
+	subscriptionCollection, err := subscription.GetSubscriptionCollectionModel()
+	if err != nil {
+		fmt.Println(env.ErrorDispatch(err))
+	}
+	currentCart, err := cart.GetCartModel()
+	if err != nil {
+		fmt.Println(env.ErrorDispatch(err))
+	}
+
+	for _, currentSubscription := range subscriptionCollection.ListSubscriptions() {
+		for _, subscriptionItem := range currentSubscription.GetItems() {
+			updatedOptions := make(map[string]interface{})
+			// Labels where used as a key for options key: value, so we will convert both of them
+			for optionKey, optionValue := range subscriptionItem.Options {
+				updatedOptions[utils.StrToSnakeCase(optionKey)] = utils.StrToSnakeCase(utils.InterfaceToString(optionValue))
+			}
+			subscriptionItem.Options = updatedOptions
+			if _, err = currentCart.AddItem(subscriptionItem.ProductID, subscriptionItem.Qty, subscriptionItem.Options); err != nil {
+				fmt.Println(env.ErrorDispatch(err))
+				fmt.Println(subscriptionItem.Options)
+			}
+		}
+
+		err = currentSubscription.Save()
+		if err != nil {
+			fmt.Println(env.ErrorDispatch(err))
+		}
+	}
 }
 
 // ConvertProductOptionsToSnakeCase updates option keys for product to case_snake
